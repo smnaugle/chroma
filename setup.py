b4cc1bca500d6957dbd931556d99f85527d27a30
smnaugle <smnaugle@sas.upenn.edu>
2025-05-13 21:05:50 -0400

    Use ruff to format, formatting changes for setup.py

--- setup.py
+++ setup.py
@@ -2,19 +2,20 @@ from setuptools import setup, find_packages, Extension
 import subprocess
 import os
 
-libraries = ['boost_python','boost_numpy']
+libraries = ['boost_python', 'boost_numpy']
 extra_objects = []
 
 if 'VIRTUAL_ENV' in os.environ:
     # use local copy of boost libs
-    boost_lib = os.path.join(os.environ['VIRTUAL_ENV'],'lib','libboost_python.so')
+    boost_lib = os.path.join(os.environ['VIRTUAL_ENV'], 'lib', 'libboost_python.so')
     if os.path.exists(boost_lib):
         extra_objects.append(boost_lib)
         libraries.remove('boost_python')
-    boost_lib = os.path.join(os.environ['VIRTUAL_ENV'],'lib','libboost_numpy.so')
+    boost_lib = os.path.join(os.environ['VIRTUAL_ENV'], 'lib', 'libboost_numpy.so')
     if os.path.exists(boost_lib):
         extra_objects.append(boost_lib)
         libraries.remove('boost_numpy')
+
 
 def check_output(*popenargs, **kwargs):
     if 'stdout' in kwargs:
@@ -23,22 +24,23 @@ def check_output(*popenargs, **kwargs):
     output, unused_err = process.communicate()
     retcode = process.poll()
     if retcode:
-        cmd = kwargs.get("args")
+        cmd = kwargs.get('args')
         if cmd is None:
             cmd = popenargs[0]
         raise subprocess.CalledProcessError(retcode, cmd, output=output)
     return output.decode('utf-8') if type(output) != str else output
 
-geant4_cflags = check_output(['geant4-config','--cflags']).split()
-geant4_libs = check_output(['geant4-config','--libs']).split()
+
+geant4_cflags = check_output(['geant4-config', '--cflags']).split()
+geant4_libs = check_output(['geant4-config', '--libs']).split()
 # For GEANT4.9.4 built without cmake
 try:
-    clhep_libs = check_output(['clhep-config','--libs']).split()
+    clhep_libs = check_output(['clhep-config', '--libs']).split()
 except OSError:
     clhep_libs = []
 
 
-include_dirs=['src']
+include_dirs = ['src']
 
 #####
 
@@ -46,50 +48,62 @@ if 'VIRTUAL_ENV' in os.environ:
     include_dirs.append(os.path.join(os.environ['VIRTUAL_ENV'], 'include'))
 try:
     import numpy.distutils
+
     include_dirs += numpy.distutils.misc_util.get_numpy_include_dirs()
 except:
-    pass # if numpy doesn't exist yet
+    pass  # if numpy doesn't exist yet
 
 # Install cuda files
 cuda_files = []
-for root, dirs, files in os.walk("chroma/"):
+for root, dirs, files in os.walk('chroma/'):
     for file in files:
-        if file.endswith(('.cu','.h')):
+        if file.endswith(('.cu', '.h')):
             cuda_files.append(os.path.join(root, file))
-data_files = [
-    ('chroma/cuda', cuda_files)
-]
+data_files = [('chroma/cuda', cuda_files)]
 
 setup(
-    name = 'Chroma',
-    version = '0.5',
-    packages = find_packages(),
+    name='Chroma',
+    version='0.5',
+    packages=find_packages(),
     include_package_data=True,
     data_files=data_files,
-    scripts = ['bin/chroma-sim', 'bin/chroma-cam',
-               'bin/chroma-geo', 'bin/chroma-bvh',
-               'bin/chroma-server'],
-    ext_modules = [
-        Extension('chroma.generator._g4chroma',
-                  ['src/G4chroma.cc','src/GLG4Scint.cc'],
-                  include_dirs=include_dirs,
-                  extra_compile_args=['--std=c++11']+geant4_cflags,
-                  extra_link_args=geant4_libs+clhep_libs,
-                  extra_objects=extra_objects,
-                  libraries=libraries,
-                  ),
-        Extension('chroma.generator.mute',
-                  ['src/mute.cc'],
-                  include_dirs=include_dirs,
-                  extra_compile_args=geant4_cflags,
-                  extra_link_args=geant4_libs+clhep_libs,
-                  extra_objects=extra_objects,
-                  libraries=libraries),
-        ],
- 
-    setup_requires = [],
-    install_requires = ['uncertainties','pyzmq', 'pycuda','pytools==2022.1.2',
-                        'numpy>=1.6', 'pygame', 'nose', 'sphinx'],
-    #test_suite = 'nose.collector',
-    
+    scripts=[
+        'bin/chroma-sim',
+        'bin/chroma-cam',
+        'bin/chroma-geo',
+        'bin/chroma-bvh',
+        'bin/chroma-server',
+    ],
+    ext_modules=[
+        Extension(
+            'chroma.generator._g4chroma',
+            ['src/G4chroma.cc', 'src/GLG4Scint.cc'],
+            include_dirs=include_dirs,
+            extra_compile_args=['--std=c++11'] + geant4_cflags,
+            extra_link_args=geant4_libs + clhep_libs,
+            extra_objects=extra_objects,
+            libraries=libraries,
+        ),
+        Extension(
+            'chroma.generator.mute',
+            ['src/mute.cc'],
+            include_dirs=include_dirs,
+            extra_compile_args=geant4_cflags,
+            extra_link_args=geant4_libs + clhep_libs,
+            extra_objects=extra_objects,
+            libraries=libraries,
+        ),
+    ],
+    setup_requires=[],
+    install_requires=[
+        'uncertainties',
+        'pyzmq',
+        'pycuda',
+        'pytools==2022.1.2',
+        'numpy>=1.6',
+        'pygame',
+        'nose',
+        'sphinx',
+    ],
+    # test_suite = 'nose.collector',
 )
